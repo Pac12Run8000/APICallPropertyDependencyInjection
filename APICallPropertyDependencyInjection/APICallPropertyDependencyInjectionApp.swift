79530706165a100197e11c7b59f92ccc3d116dd0
Pac12Run8000 <gangstar78666@>
2024-04-14 20:05:02 -0700

    Initial Commit

---
+++ APICallPropertyDependencyInjection/APICallPropertyDependencyInjectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  APICallPropertyDependencyInjectionApp.swift
+//  APICallPropertyDependencyInjection
+//
+//  Created by Norbert Grover on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct APICallPropertyDependencyInjectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
